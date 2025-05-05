3acafd294f3e359ec14bd3fe6354cec4a492649f
Pavan <117636377+Pavan-folder@users.noreply.github.com>
2025-05-05 21:46:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [],
   server: {
-    port: 3000 // You can change this port if necessary
+    port: 3000 // You can change this port if needed
   }
 })
+
