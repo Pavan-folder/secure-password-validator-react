ba55a701d0ce1c9a34225adcefa63bbfb176e248
Pavan <117636377+Pavan-folder@users.noreply.github.com>
2025-05-05 20:52:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  base: '/secure-password-validator-react/', // 👈 your repo name here
+  base: '/',
   plugins: [react()],
 })
+
 
